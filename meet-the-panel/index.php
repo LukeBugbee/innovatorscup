996f3ad75a6c6508a343b98ff4996a0ac1d08934
dylanguerber <dylan@pixelsmith.co>
2018-09-11 10:58:55 -0500

    update partners panels
    - animate in, update hover

--- meet-the-panel/index.php
+++ meet-the-panel/index.php
@@ -3,15 +3,19 @@
 	<section class="mod mod--panel-members">
 		<div class="container">
 			<div class="panel">
+
 				<?php foreach ($panelMembers as $member): ?>
-					<div class="panel__member">
+
+					<div class="panel__member animate animate--fade-in">
 						<figure style="background-image: url(/assets/img/panel/<?= $member['img'] ?>);"></figure>
 						<div class="content">
 							<h2><?= $member['name'] ?></h2>
 							<p><?= $member['title'] ?></p>
 						</div><!-- /.content -->
 					</div><!-- /.panel__member -->
+
 				<?php endforeach ?>
+
 			</div><!-- /.panel -->
 		</div><!-- /.container -->
 	</section><!-- /.mod mod--panel-members -->
