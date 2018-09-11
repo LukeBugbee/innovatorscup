7a580af3af914e7d67d4070f6b05e3ef48b4ae09
dylanguerber <dylan@pixelsmith.co>
2018-09-11 10:52:02 -0500

    project examples
    - update card hover, animation, and links

--- project-examples/index.php
+++ project-examples/index.php
@@ -2,20 +2,31 @@
 	<section class="mod mod--projects">
 		<div class="container">
 			<div class="projects grid">
+
 				<?php foreach ($projects as $project): ?>
-				<div class="project item">
-					<div class="content">
-						<figure class="project__figure project__figure--<?= str_replace('.png', '', $project['logo']) ?>" style="background-image: url(/assets/img/projects/<?= $project['logo'] ?>)"></figure><!-- /.project__figure -->
-						<article class="project__content">
-							<p>
-								<strong class="white"><?= $project['name'] ?></strong> | 
-								<a href="http://<?= $project['url'] ?>" target="_blank"><?= $project['url'] ?></a>
-							</p>
-							<p><?= $project['blurb'] ?></p>
-						</article><!-- /.project__content -->
-					</div><!-- /.content -->
-				</div><!-- /.project -->
+
+					<div class="project item animate animate--fade-in">
+						<div class="content">
+
+							<a href="http://<?= $project['url'] ?>" target="_blank">
+								<figure class="project__figure project__figure--<?= str_replace('.png', '', $project['logo']) ?>" style="background-image: url(/assets/img/projects/<?= $project['logo'] ?>)"></figure><!-- /.project__figure -->
+							</a>
+
+							<article class="project__content">
+								<p>
+									<strong class="white"><?= $project['name'] ?></strong> |
+									<a href="http://<?= $project['url'] ?>" target="_blank"><?= $project['url'] ?></a>
+								</p>
+								<p><?= $project['blurb'] ?></p>
+
+								<a class="cover-link" href="http://<?= $project['url'] ?>" target="_blank">
+							</article>
+
+						</div>
+					</div>
+
 				<?php endforeach ?>
+
 			</div><!-- /.projects -->
 		</div><!-- /.container -->
 	</section><!-- /.mod mod--panel-members -->
