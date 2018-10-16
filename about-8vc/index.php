f7f777a5b6fbc099ed849b0d6313edff7b54d339
Mike Voermans <mikevoermans@Mikes-MacBook-Pro-2.local>
2018-10-16 08:57:06 -0500

    adding links

--- about-8vc/index.php
+++ about-8vc/index.php
@@ -3,7 +3,7 @@
 	<article class="mod mod--post">
 
 		<div class="container container--small animate animate--fade-in">
-			<p class="lead">Market-driven innovation is the most powerful solution to many social problems and the mutual benefit framework of entrepreneurship within free markets has lifted and enriched the lives of billions of people. But focusing solely on profit misses major dimensions of our moral existence. We believe that true wealth and happiness consists in serving those in need, bettering our communities, and inspiring others to do the same.</p><!-- /.lead -->
+			<p class="lead">Market-driven innovation is the most powerful solution to many social problems and the mutual benefit framework of entrepreneurship within free markets has lifted and enriched the lives of billions of people. But focusing solely on profit misses major dimensions of our moral existence. <a target="_blank" href="https://8vc.com/resources-overview/">We believe</a> that true wealth and happiness consists in serving those in need, bettering our communities, and inspiring others to do the same.</p><!-- /.lead -->
 			<hr class="divider" />
 			<p>Today we face a <a target="_blank" href="https://medium.com/8vc-news/a-deficit-of-leadership-38bb888a797f">deficit of leadership</a>. North America’s best students must rise to the occasion and pursue inspirational projects that directly touch the lives of others. The goal of 8VC’s Social Innovator’s Cup is to fund, mentor, and accelerate the best entrepreneurial projects.</p>
 			<p>The 8VC Social Innovator’s Cup will fund radical new initiatives that create “shared value” by expanding opportunity for all, raising standards of living, and strengthening civic bonds. Projects that harness market forces and community compassion can improve lives at scale.</p>
@@ -22,7 +22,7 @@
 				Joe Lonsdale is founding partner of 8VC. He is the top ranked venture investor in the world under 40 and was the youngest member of Forbes’ Midas list in 2016 and 2017. Joe and the partnership at 8VC know firsthand the ability for young people to affect incredible economic and social change, and we’re excited to enable current students to bring their most innovative social impact ideas to life.
 			</p>
 
-			<p>Learn more about us through our <a href="https://8vc.com/resources/whitepapers/">8VC whitepapers</a> and recent <a href="https://8vc.com/resources/blog/">blog posts</a>.</p>
+			<p>Learn more about us through our <a target="_blank" href="https://8vc.com/resources/whitepapers/">8VC whitepapers</a> and recent <a target="_blank" href="https://8vc.com/resources/blog/">blog posts</a>.</p>
 
 		</div><!-- /.container container--small -->
 	</article><!-- /.post -->
